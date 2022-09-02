fb772e59f55d625668dc3f68ce7e9c1a9337e253
SETHUMADHAVAN-ARIVAZHAGAN <97796904+SETHUMADHAVAN-ARIVAZHAGAN@users.noreply.github.com>
2022-09-02 23:38:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,13 +26,14 @@ print(df.head(10))
 # df = pd.DataFrame(ws.get_all_records())
 df=pd.DataFrame(df)
 
+df['Date and Time']=df['created_at']
 df['Humidity (M - ON) ']=df['field5']
 df['Humidity (M - OFF)']=df['field6']
 df['Moisture (M - ON)']=df['field1']
 df['Moisture (M - OFF)']=df['field2']
 df['Temperature (M - ON)']=df['field3']
 df['Temperature (M - OFF)']=df['field4']
-df['Date and Time']=df['created_at']
+
 
 df1=df.drop(['field1','entry_id','created_at','field2','field3','field4','field5','field6'],axis=1)
 # humidity_1=h['field5'].iloc[0]
